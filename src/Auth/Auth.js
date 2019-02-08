22c70c9c76dfd1d00046e7091943d90484a58057
Alexandre Torres Sampaio <at.sampaio@gmail.com>
2019-02-08 13:41:50 -0200

    Silent Authentication and Token

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -2,6 +2,13 @@ import auth0 from "auth0-js";
 
 const REDIRECT_ON_LOGIN = "redirect_on_login";
 
+// Stored outside class since private
+// eslint-disable-next-line
+let _idToken = null;
+let _accessToken = null;
+let _scopes = null;
+let _expiresAt = null;
+
 export default class Auth {
   constructor(history) {
     this.history = history;
@@ -47,28 +54,18 @@ export default class Auth {
   setSession = authResult => {
     // Set the time that the access token will expire
     // Calculating unix epoque date it will exprire
-    const expiresAt = JSON.stringify(
-      authResult.expiresIn * 1000 + new Date().getTime()
-    );
-    const scopes = authResult.scope || this.requestedScopes || "";
+    this._expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
+    this._scopes = authResult.scope || this.requestedScopes || "";
 
-    localStorage.setItem("access_token", authResult.accessToken);
-    localStorage.setItem("id_token", authResult.idToken);
-    localStorage.setItem("expires_at", expiresAt);
-    localStorage.setItem("scopes", JSON.stringify(scopes));
+    this._accessToken = authResult.accessToken;
+    this._idToken = authResult.idToken;
   };
 
   isAuthenticated() {
-    const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
-    return new Date().getTime() < expiresAt;
+    return new Date().getTime() < _expiresAt;
   }
 
   logout = () => {
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("id_token");
-    localStorage.removeItem("expires_at");
-    localStorage.removeItem("scopes");
-    this.userProfile = null;
     // This way we only log out locally. It is usefull for Single sign on  applications.
     // When more then one application uses the same subscribition on auth0
     //this.history.push("/");
@@ -79,11 +76,10 @@ export default class Auth {
   };
 
   getAccessToken = () => {
-    const accessToken = localStorage.getItem("access_token");
-    if (!accessToken) {
+    if (!_accessToken) {
       throw new Error("No access token found.");
     }
-    return accessToken;
+    return _accessToken;
   };
 
   getProfile = cb => {
@@ -96,9 +92,7 @@ export default class Auth {
   };
 
   userHasScopes(scopes) {
-    const grantedScopes = (
-      JSON.parse(localStorage.getItem("scopes")) || ""
-    ).split(" ");
+    const grantedScopes = (_scopes || "").split(" ");
     return scopes.every(scope => grantedScopes.includes(scope));
   }
 }
